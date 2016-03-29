83302c33964521407050815c1cc1186d53647221
mojaero <mo.malie@hotmail.com>
2016-03-29 23:38:09 +0200

    Project baseline

---
+++ DoodleChat/src/DoodleChat/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoodleChat
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            // committ number 1
+        }
+    }
+}
